78a289c2812b1edb4bfe39d7169c1ccc8cb59a7b
HellenWeb <artemkondratyev23@gmail.com>
2022-05-14 15:57:02 +0300

    Added changes in ipscan.py file

--- ipscan.py
+++ ipscan.py
@@ -1,4 +1,3 @@
-
 # Modules
 
 import requests
@@ -7,44 +6,64 @@ from rich.console import Console
 import argparse
 from time import sleep
 
-# Default Variebles
-
-console = Console()
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument ("--ip", help= "IP address", type=str, dest='target', required=True )
-
-args = parser.parse_args()
-
-addr = args.target
-
-# Logo
-
-logo = """
-
-██╗██████╗░░██████╗░█████╗░░█████╗░███╗░░██╗
-██║██╔══██╗██╔════╝██╔══██╗██╔══██╗████╗░██║
-██║██████╔╝╚█████╗░██║░░╚═╝███████║██╔██╗██║
-██║██╔═══╝░░╚═══██╗██║░░██╗██╔══██║██║╚████║
-██║██║░░░░░██████╔╝╚█████╔╝██║░░██║██║░╚███║
-╚═╝╚═╝░░░░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝ v. 0.1.0
-"""
-
-console.print(logo, style="red")
-
 # Logic
 
-try:
-    ip = requests.get(f"http://ip-api.com/json/{addr}").json()
-    with console.status("Show info...") as status:
-        sleep(1)
-    console.print(f'\n\nCountry: {ip["country"]}\n', style="bold blue")
-    console.print(f'Region: {ip["regionName"]}\n', style="bold blue")
-    console.print(f'City: {ip["city"]}\n', style="bold blue")
-    console.print(f'Time Zone: {ip["timezone"]}\n', style="bold blue")
-    with open("ipInfo.txt", "w") as f:
-        f.write(f'Country: {ip["country"]}\nRegion: {ip["regionName"]}\nCity: {ip["city"]}\nTime Zone: {ip["timezone"]}')
-    console.print("All information ip ([bold green]ipInfo.txt[/bold green])", style="bold red")
-except requests.exceptions.ConnectionError as e:
-    console.print("[*] No internet connection", style="bold red")
+
+class IPScan:
+    def __init__(self, addr):
+        self.ip = addr
+
+    def start(self):
+        try:
+            ip = requests.get(f"http://ip-api.com/json/{self.ip}").json()
+            with console.status("Show info...") as status:
+                sleep(1)
+            console.print(f'\n\nCountry: {ip["country"]}\n', style="bold blue")
+            console.print(f'Region: {ip["regionName"]}\n', style="bold blue")
+            console.print(f'City: {ip["city"]}\n', style="bold blue")
+            console.print(f'Time Zone: {ip["timezone"]}\n\n', style="bold blue")
+            with open("ipInfo.txt", "w") as f:
+                f.write(
+                    f'Country: {ip["country"]}\nRegion: {ip["regionName"]}\nCity: {ip["city"]}\nTime Zone: {ip["timezone"]}'
+                )
+                console.print(
+                    "All information ip ([bold green]ipInfo.txt[/bold green])",
+                    style="bold red",
+                )
+        except requests.exceptions.ConnectionError as e:
+            console.print("[*] No internet connection", style="bold red")
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+
+    console = Console()
+
+    logo = """
+
+    █  ██████╗░░██████╗░█████╗░░█████╗░███╗░░██╗
+    ██║██╔══██╗██╔════╝██╔══██╗██╔══██╗████╗░██║
+    ██║██████╔╝╚█████╗░██║░░╚═╝███████║██╔██╗██║
+    ██║██╔═══╝░░╚═══██╗██║░░██╗██╔══██║██║╚████║
+    ██║██║░░░░░██████╔╝╚█████╔╝██║░░██║██║░╚███║
+    ╚═╝╚═╝░░░░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝ v. 0.1.0
+
+               Copyright by. Hellen
+    """
+
+    console.print(logo, style="red")
+
+    # Parser
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ip", help="IP address", type=str, dest="target", required=True
+    )
+    args = parser.parse_args()
+    addr = args.target
+
+    # Class
+
+    ipscan = IPScan(addr)
+    ipscan.start()
+    sys.exit(1)
